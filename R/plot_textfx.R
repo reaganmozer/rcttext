8c23f077c8b0ff6f884681019527c124fae85555
Reagan Mozer <reaganmro@gmail.com>
2023-08-30 12:08:11 -0400

    Update plotting functions

--- R/plot_textfx.R
+++ R/plot_textfx.R
@@ -23,7 +23,7 @@ plot_textfx= function(out,  alpha=0.05, cols=F, group=T, xlim=NULL, ...){
   if (group){
     out1$ord[out1$name%in%c("Analytic","Authentic","Clout","Tone")]=1
     out1$ord[out1$name%in%c("WC","WPS","TTR","XXX","Sixltr",'Flesch.Kincaid', "Flesch",
-                       "R","ARI")]=2
+                            "R","ARI")]=2
   }
   data("dimnames")
   out1 = merge(out1, dimnames, by="name",all.x=T)
@@ -42,8 +42,8 @@ plot_textfx= function(out,  alpha=0.05, cols=F, group=T, xlim=NULL, ...){
   }
 
   plotrix::plotCI(x=out1$est, y=1:nrow(out1),ui=out1$UL, li=out1$LL,
-         err="x",yaxt="n",ylab="",xlab="Standardized difference in means (95% CI)",
-         pch=NA,scol="white",xlim=c(xl-0.05,xr+0.05),xaxt="n",...)
+                  err="x",yaxt="n",ylab="",xlab="Standardized difference in means (95% CI)",
+                  pch=NA,scol="white",xlim=c(xl-0.05,xr+0.05),xaxt="n",...)
   axis(side=1,at=seq(xl, xr, by=0.2),labels=sprintf("%0.1f",seq(xl,xr,0.2)))
   abline(v=0,lty=2)
   if (length(unique(out1$ord))>2){
@@ -56,8 +56,8 @@ plot_textfx= function(out,  alpha=0.05, cols=F, group=T, xlim=NULL, ...){
   linecols = ifelse(out1$p.adj<=alpha, 1,0.5)
   ptwt = ifelse(out1$p.adj<=alpha, 0.75, 0.1)
   plotrix::plotCI(x=out1$est, y=1:nrow(out1), ui=out1$UL, li=out1$LL,
-         err="x",add=T,scol=alpha("black",linecols), col=alpha("black",linecols),
-         pch=21, pt.bg=alpha(ptcols,ptwt), cex=1.3,lwd=1.25)
+                  err="x",add=T,scol=alpha("black",linecols), col=alpha("black",linecols),
+                  pch=21, pt.bg=alpha(ptcols,ptwt), cex=1.3,lwd=1.25)
 
   which.sigs = out1$p.adj<=alpha
   y1 = (1:nrow(out1))[!which.sigs]
